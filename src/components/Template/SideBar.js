40250eb0d179137c042a930dccc67abe34b02d2d
Steven Kam <skam@stevens-mbp-2.lan>
2022-12-23 22:48:38 -0800

    sidebar, welcome page, name change

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -19,14 +19,16 @@ const SideBar = () => (
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-        at <a href="https://matroid.com">Matroid</a>
-        , <a href="https://planet.com">Planet</a>
-        , <a href="https://planetaryresources.com">Planetary Resources</a>
-        , <a href="https://facebook.com">Facebook</a>
-        , and <a href="https://seds.org">SEDS</a>.
+      <p>Hi, I&apos;m Steven. I like building things.
+        I am a <a href="https://www.cs.cmu.edu/">Carnegie Mellon</a> student studying Computer Science
+        I am currently a SDE Intern at <a href="https://www.amazon.com/">Amazon</a>, and before Amazon
+        I was at <a href="https://www.optum.com//">Optum</a>
+        , <a href="https://www.cmuautonomous.org/">CMU CAR</a>
+        , and <a href="https://ipsf.net/irvine-cubesat/#irvine-cubesat">Irvine CubeSat</a>.
+        I have always been fascinated by the power of technology to solve problems and make
+        a positive impact on the world. In my studies, I have learned a wide range of programming
+        languages and technologies, and I am constantly seeking out new challenges and opportunities
+        to expand my skills and knowledge.
       </p>
       <ul className="actions">
         <li>
@@ -37,7 +39,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Steven Kam <Link to="/">stevenkam.com</Link>.</p>
     </section>
   </section>
 );
--- src/data/contact.js
+++ src/data/contact.js
@@ -1,8 +1,6 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
-import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
@@ -10,37 +8,27 @@ import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 
 const data = [
   {
-    link: 'https://github.com/mldangelo',
-    label: 'Github',
-    icon: faGithub,
-  },
-  {
-    link: 'https://facebook.com/md',
-    label: 'Facebook',
-    icon: faFacebookF,
-  },
-  {
-    link: 'https://www.instagram.com/dangelosaurus/',
-    label: 'Instagram',
-    icon: faInstagram,
-  },
-  {
-    link: 'https://www.linkedin.com/in/michaelldangelo',
+    link: 'https://www.linkedin.com/in/segmentation-fault/',
     label: 'LinkedIn',
     icon: faLinkedinIn,
   },
   {
-    link: 'https://angel.co/michael-d-angelo',
-    label: 'Angel List',
-    icon: faAngellist,
+    link: 'https://github.com/SKam23',
+    label: 'Github',
+    icon: faGithub,
   },
   {
-    link: 'https://twitter.com/dangelosaurus',
+    link: 'https://www.instagram.com/steven.kammm/',
+    label: 'Instagram',
+    icon: faInstagram,
+  },
+  {
+    link: 'https://twitter.com/S_Kammm',
     label: 'Twitter',
     icon: faTwitter,
   },
   {
-    link: 'mailto:michael.l.dangelo@gmail.com',
+    link: 'mailto:stevenkam@cmu.edu',
     label: 'Email',
     icon: faEnvelope,
   },
