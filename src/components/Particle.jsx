0ca9241eaf98703048d1fa8a7919f908ff38eb62
Steven Kam <skam@Stevens-MacBook-Pro-2.local>
2022-12-26 16:21:12 -0800

    particles error catching

--- src/components/Particle.jsx
+++ src/components/Particle.jsx
@@ -1,18 +1,29 @@
 import { useCallback } from 'react';
 import Particles from 'react-tsparticles';
 import { loadFull } from 'tsparticles';
+
 /* eslint-disable */
 const Particle = () => {
   const particlesInit = useCallback(async engine => {
-    console.log(engine);
+    try{
+      await loadFull(engine);
+    } catch (error){
+      return null
+    }
+    // console.log(engine);
     // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
     // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
+    // await loadFull(engine);
   }, []);
 
   const particlesLoaded = useCallback(async container => {
-    await console.log(container);
+    try{
+      await loadFull(container);
+    } catch (error){
+      return null
+    }
+    // await console.log(container);
   }, []);
 
   return (
